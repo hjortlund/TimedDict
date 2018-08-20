d87efc48a9796502740f2dbacd4a8638ff1627c4
Christoffer Hjortlund <hjortlund@gmail.com>
2018-08-20 14:33:34 +0200

    Initial commit!

---
+++ example.py
@@ -0,0 +1,48 @@
+import time
+from TimedDict.timeddict import TimedDict
+
+events_window = TimedDict()
+
+now = time.time()
+
+# Assign values like a normal dict like:
+events_window[now] = 'value_1'
+events_window[now + 1] = 'value_2'
+
+# ...or like:
+events_window.update({now + 2: {'values': {'value_3', 'value_4'}}})
+
+print('Raw data:')
+print(events_window)
+
+# NOTE:
+# As the TimedDict has a thread running purging old elements, it's important to ether
+# use the protect() or pause() followed by a resume() when iterating.
+
+# Automatic by the use of context manager, protect() approach
+with events_window.protect():
+    print('\n- protect()')
+    for event in events_window:
+        print(event)
+
+# Manual setting, pause() and resume() approach
+events_window.pause()
+print('\n- pause() followed by resume()')
+
+for event in events_window:
+    print(event)
+
+events_window.resume()
+
+# TTL example
+print('\nLength of the TimedDict: {}'.format(len(events_window)))
+print(events_window)
+time.sleep(1.1)
+print(events_window)
+time.sleep(1)
+print(events_window)
+time.sleep(1)
+print(events_window)
+
+# Gracefully stop the purge thread
+events_window.stop()
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='TimedDict',
+    version='0.1.0',
+    packages=['venv.Lib.site-packages.pip-9.0.1-py3.5.egg.pip', 'venv.Lib.site-packages.pip-9.0.1-py3.5.egg.pip.req', 'venv.Lib.site-packages.pip-9.0.1-py3.5.egg.pip.vcs', 'venv.Lib.site-packages.pip-9.0.1-py3.5.egg.pip.utils', 'venv.Lib.site-packages.pip-9.0.1-py3.5.egg.pip.compat', 'venv.Lib.site-packages.pip-9.0.1-py3.5.egg.pip.models', 'venv.Lib.site-packages.pip-9.0.1-py3.5.egg.pip._vendor', 'venv.Lib.site-packages.pip-9.0.1-py3.5.egg.pip._vendor.distlib', 'venv.Lib.site-packages.pip-9.0.1-py3.5.egg.pip._vendor.distlib._backport', 'venv.Lib.site-packages.pip-9.0.1-py3.5.egg.pip._vendor.colorama', 'venv.Lib.site-packages.pip-9.0.1-py3.5.egg.pip._vendor.html5lib', 'venv.Lib.site-packages.pip-9.0.1-py3.5.egg.pip._vendor.html5lib._trie', 'venv.Lib.site-packages.pip-9.0.1-py3.5.egg.pip._vendor.html5lib.filters', 'venv.Lib.site-packages.pip-9.0.1-py3.5.egg.pip._vendor.html5lib.treewalkers', 'venv.Lib.site-packages.pip-9.0.1-py3.5.egg.pip._vendor.html5lib.treeadapters',
+              'venv.Lib.site-packages.pip-9.0.1-py3.5.egg.pip._vendor.html5lib.treebuilders', 'venv.Lib.site-packages.pip-9.0.1-py3.5.egg.pip._vendor.lockfile', 'venv.Lib.site-packages.pip-9.0.1-py3.5.egg.pip._vendor.progress', 'venv.Lib.site-packages.pip-9.0.1-py3.5.egg.pip._vendor.requests', 'venv.Lib.site-packages.pip-9.0.1-py3.5.egg.pip._vendor.requests.packages', 'venv.Lib.site-packages.pip-9.0.1-py3.5.egg.pip._vendor.requests.packages.chardet', 'venv.Lib.site-packages.pip-9.0.1-py3.5.egg.pip._vendor.requests.packages.urllib3', 'venv.Lib.site-packages.pip-9.0.1-py3.5.egg.pip._vendor.requests.packages.urllib3.util', 'venv.Lib.site-packages.pip-9.0.1-py3.5.egg.pip._vendor.requests.packages.urllib3.contrib', 'venv.Lib.site-packages.pip-9.0.1-py3.5.egg.pip._vendor.requests.packages.urllib3.packages', 'venv.Lib.site-packages.pip-9.0.1-py3.5.egg.pip._vendor.requests.packages.urllib3.packages.ssl_match_hostname', 'venv.Lib.site-packages.pip-9.0.1-py3.5.egg.pip._vendor.packaging',
+              'venv.Lib.site-packages.pip-9.0.1-py3.5.egg.pip._vendor.cachecontrol', 'venv.Lib.site-packages.pip-9.0.1-py3.5.egg.pip._vendor.cachecontrol.caches', 'venv.Lib.site-packages.pip-9.0.1-py3.5.egg.pip._vendor.webencodings', 'venv.Lib.site-packages.pip-9.0.1-py3.5.egg.pip._vendor.pkg_resources', 'venv.Lib.site-packages.pip-9.0.1-py3.5.egg.pip.commands', 'venv.Lib.site-packages.pip-9.0.1-py3.5.egg.pip.operations', 'TimedDict'],
+    url='',
+    license='MIT',
+    author='Christoffer Hjortlund',
+    author_email='hjortlund@gmail.com',
+    description='Extending the dict data type, with a rolling window feature, that automatically purge elements falling outside the defined window.'
+)
